e86784c97729aff88fe5f8ec6923cd663079d20d
Mathieu Bonhommeau <mat.bonhommeau85@gmail.com>
2022-12-20 23:34:30 +0100

    feat: add menu burger

---
+++ modules/MenuBurger/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { pushRotate as Menu } from "react-burger-menu";
+
+function MenuBurger({ pageWrapId, outerContainerId }) {
+    return (
+        <Menu pageWrapId={pageWrapId} outerContainerId={outerContainerId}>
+            <a id="home" className="menu-item" href="/">
+                Home
+            </a>
+            <a id="about" className="menu-item" href="/about">
+                About
+            </a>
+            <a id="contact" className="menu-item" href="/contact">
+                Contact
+            </a>
+            <a
+                /* onClick={this.showSettings} */ className="menu-item--small"
+                href=""
+            >
+                Settings
+            </a>
+        </Menu>
+    );
+}
+
+export default MenuBurger;
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import SpeechManager from "../modules/SpeechManager";
-import { useEffect } from "react";
+import MenuBurger from "../modules/MenuBurger";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,26 +18,35 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className="flex flex-col max-w-screen-md min-h-screen items-center mx-auto px-6">
-                <div className={"flex flex-col items-center w-full mb-10"}>
-                    <Image
-                        className="w-128 sm:w-96"
-                        src="/ia.gif"
-                        alt="Next.js Logo"
-                        width={820}
-                        height={1104}
-                        priority={true}
-                    />
-                    <div className="text-white text-3xl text-3xl md:text-4xl my-0 primary-title text-center">
-                        JE VOUS ECOUTE !
+            <div id="outer-container">
+                <MenuBurger
+                    pageWrapId={"page-wrap"}
+                    outerContainerId={"outer-container"}
+                />
+                <main
+                    id="page-wrap"
+                    className="flex flex-col max-w-screen-md min-h-screen items-center mx-auto px-6"
+                >
+                    <div className={"flex flex-col items-center w-full mb-10"}>
+                        <Image
+                            className="w-128 sm:w-96"
+                            src="/ia.gif"
+                            alt="Next.js Logo"
+                            width={820}
+                            height={1104}
+                            priority={true}
+                        />
+                        <div className="text-white text-3xl text-3xl md:text-4xl my-0 primary-title text-center">
+                            JE VOUS ECOUTE !
+                        </div>
                     </div>
-                </div>
-                <div className="w-full px-3">
-                    <div className="flex flex-col">
-                        <SpeechManager />
+                    <div className="w-full px-3">
+                        <div className="flex flex-col">
+                            <SpeechManager />
+                        </div>
                     </div>
-                </div>
-            </main>
+                </main>
+            </div>
         </div>
     );
 }
